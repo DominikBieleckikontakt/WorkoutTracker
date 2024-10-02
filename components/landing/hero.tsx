d2c29a41e7dc8fce498a7fd2cfb5e6426aa104fa
DominikBieleckikontakt <dominikbielecki.kontakt@gmail.com>
2024-10-02 18:29:13 +0200

    hero titles added

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Button } from "../ui/button";
 
 const Hero = () => {
-  return <div>hero</div>;
+  return (
+    <div>
+      <div className="max-w-[1100px] mx-5 md:mx-auto">
+        <h1 className="font-thin text-5xl text-center mt-48 md:text-6xl lg:text-8xl ">
+          Your Health, <span className="font-semibold">Simplified</span>
+        </h1>
+        <p className="text-center mt-5 text-sm md:text-md lg:text-lg text-gray-500">
+          Track your workouts, meals and progress with our intuitive dashboard.
+        </p>
+        <p className="text-center mt-1 text-sm md:text-md lg:text-lg text-gray-500">
+          Stay motivated and reach your fitness goals faster.
+        </p>
+        <div className="flex gap-5 justify-center mt-8">
+          <Button className="rounded-xl" size="lg">
+            Get started
+          </Button>
+          <Button variant="outline" className="rounded-xl" size="lg">
+            Contact Sales
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -2,169 +2,166 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Button } from "./button"; // Assuming Button is a custom component
+import { Transition } from "@headlessui/react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="p-10 px-16 border-b-2 border-gray-200 z-50 sticky top-0 bg-background/5 backdrop-blur-sm">
-      <div className="flex justify-between items-center">
-        {/* Logo */}
-        <div className="text-3xl font-semibold">
-          <Link href="#">
-            Fitly<span className="text-primary">.</span>
-          </Link>
-        </div>
-
-        {/* Hamburger Menu Button for medium screens and below */}
-        <div className="md:hidden">
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            className="focus:outline-none"
-          >
-            {/* Change the icon based on isOpen state */}
-            {isOpen ? (
-              <svg
-                className="w-8 h-8 text-black"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-8 h-8 text-black"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-
-        {/* Desktop Links */}
-        <div className="hidden md:flex md:items-center">
-          <ul className="flex gap-8">
-            <li>
-              <Link
-                href="#"
-                className="text-black hover:text-black/80 duration-300"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="text-black hover:text-black/80 duration-300"
-              >
-                Features
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="text-black hover:text-black/80 duration-300"
-              >
-                Pricing
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="text-black hover:text-black/80 duration-300"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        {/* Login Button */}
-        <div className="hidden md:block">
-          <ul>
-            <li>
-              <Button asChild className="px-8 py-6 duration-300">
-                <Link href="#" className="font-semibold text-lg">
-                  Login
-                </Link>
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      {/* Mobile Menu (Slide from Right) */}
-      <div
-        className={`fixed top-0 right-0 h-full w-3/4 bg-background shadow-lg z-40 transition-transform duration-500 ease-in-out transform ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <ul className="space-y-5 mt-5 p-5">
-          <li>
+    <nav className="bg-white/80 backdrop-blur-md w-full fixed top-0 left-0 right-0 z-50 border-b-2 border-gray-100 py-5">
+      <div className="px-16">
+        <div className="flex items-center justify-between h-16">
+          {/* Logo */}
+          <div className="flex justify-start">
+            <Link href="#" className="text-2xl font-semibold">
+              Fitly<span className="text-primary">.</span>
+            </Link>
+          </div>
+          {/* Hamburger menu (mobile) */}
+          <div className="flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="text-gray-800 hover:text-gray-500 focus:outline-none"
+              aria-controls="mobile-menu"
+              aria-expanded={isOpen}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!isOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          {/* Links and button on larger screens */}
+          <div className="hidden md:flex space-x-8 justify-center flex-1">
             <Link
               href="#"
-              className="text-black hover:text-black/80 duration-300"
+              className="text-lg font-medium text-gray-700 hover:text-gray-900"
             >
               Home
             </Link>
-          </li>
-          <li>
             <Link
               href="#"
-              className="text-black hover:text-black/80 duration-300"
+              className="text-lg font-medium text-gray-700 hover:text-gray-900"
             >
               Features
             </Link>
-          </li>
-          <li>
             <Link
               href="#"
-              className="text-black hover:text-black/80 duration-300"
+              className="text-lg font-medium text-gray-700 hover:text-gray-900"
             >
               Pricing
             </Link>
-          </li>
-          <li>
             <Link
               href="#"
-              className="text-black hover:text-black/80 duration-300"
+              className="text-lg font-medium text-gray-700 hover:text-gray-900"
+            >
+              Blog
+            </Link>
+            <Link
+              href="#"
+              className="text-lg font-medium text-gray-700 hover:text-gray-900"
             >
               Contact
             </Link>
-          </li>
-          <li>
-            <Button asChild className="px-8 py-6 duration-300 w-full text-left">
-              <Link href="#" className="font-semibold text-lg">
-                Login
+          </div>
+          <div className="hidden md:flex justify-end">
+            <Button
+              asChild
+              className="px-5 py-2 border border-transparent text-md font-medium rounded-md text-white bg-primary hover:bg-primary-dark"
+            >
+              <Link href="#" className="">
+                Get Started
               </Link>
             </Button>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-
-      {/* Overlay (Optional): To create an overlay when the mobile menu is open */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-30"
-          onClick={() => setIsOpen(false)}
-        ></div>
-      )}
+      {/* Mobile Menu */}
+      <Transition
+        show={isOpen}
+        enter="transition ease-out duration-300 transform"
+        enterFrom="translate-x-full opacity-0"
+        enterTo="translate-x-0 opacity-100"
+        leave="transition ease-in duration-300 transform"
+        leaveFrom="translate-x-0 opacity-100"
+        leaveTo="translate-x-full opacity-0"
+      >
+        <div className="md:hidden backdrop-blur-md">
+          <div className="bg-white/90 backdrop-blur-md absolute right-0 w-2/3 h-screen z-40 p-6 transition-transform duration-300 ease-in-out">
+            <div className="flex flex-col space-y-4">
+              <Link
+                href="/link1"
+                className="text-gray-800 hover:text-gray-500 link"
+              >
+                Link 1
+              </Link>
+              <Link
+                href="/link2"
+                className="text-gray-800 hover:text-gray-500 link"
+              >
+                Link 2
+              </Link>
+              <Link
+                href="/link3"
+                className="text-gray-800 hover:text-gray-500 link"
+              >
+                Link 3
+              </Link>
+              <Link
+                href="/link4"
+                className="text-gray-800 hover:text-gray-500 link"
+              >
+                Link 4
+              </Link>
+              <Link
+                href="/link5"
+                className="text-gray-800 hover:text-gray-500 link"
+              >
+                Link 5
+              </Link>
+              <Link
+                href="/button"
+                className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 link"
+              >
+                Button
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Transition>
     </nav>
   );
 };
